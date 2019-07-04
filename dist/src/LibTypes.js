bdfd6cf7fb499ab8dc3a0db4b6475c5baf311fc9
Niputi <7137178+Niputi@users.noreply.github.com>
2019-07-04 23:25:41 +0200

    ts build

--- dist/src/LibTypes.js
+++ dist/src/LibTypes.js
@@ -1,3 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiTGliVHlwZXMuanMiLCJzb3VyY2VSb290IjoiLi9zcmMvIiwic291cmNlcyI6WyJzcmMvTGliVHlwZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
--- dist/src/index.js
+++ dist/src/index.js
@@ -6,4 +6,3 @@ function SnowTransfer(token, options = {}) {
 }
 exports.SnowTransfer = SnowTransfer;
 exports.default = SnowTransfer;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiLi9zcmMvIiwic291cmNlcyI6WyJzcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxpREFBc0Q7QUFHdEQsU0FBUyxZQUFZLENBQUMsS0FBYSxFQUFFLFVBQW1CLEVBQUU7SUFDdEQsT0FBTyxJQUFJLHNCQUFZLENBQUMsS0FBSyxFQUFFLE9BQU8sQ0FBQyxDQUFDO0FBQzVDLENBQUM7QUFHUSxvQ0FBWTtBQURyQixrQkFBZSxZQUFZLENBQUMifQ==
